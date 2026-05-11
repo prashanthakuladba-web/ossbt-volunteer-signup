9266974bb9443580da4c0714bde2ff5c0ad1b2d2
Prashanth Akula <prashanthakula@Prashanths-MacBook-Air.local>
2026-05-10 22:36:32 -0400

    Fix date badge in EventCard; display all times in Eastern Time

--- components/EventCard.js
+++ components/EventCard.js
@@ -4,13 +4,13 @@ import styles from '../styles/EventCard.module.css';
 
 export default function EventCard({ event }) {
   const d = new Date(event.event_date);
-  const dayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][d.getDay()];
+  const dayName = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][d.getUTCDay()];
 
   return (
     <Link href={`/events/${event.id}`} className={styles.card}>
       <div className={styles.dateBadge}>
         <span className={styles.dayName}>{dayName}</span>
-        <span className={styles.dayNum}>{d.getDate()}</span>
+        <span className={styles.dayNum}>{d.getUTCDate()}</span>
       </div>
       <div className={styles.info}>
         <h2 className={styles.title}>{event.title}</h2>
